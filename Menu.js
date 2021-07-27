d114aef2d2c10e7d99bbde3f8d18c488cfd78a11
Laura Andrea Gómez Carmona <71355808+LauraAndreaGomezCarmona@users.noreply.github.com>
2021-07-26 23:58:48 -0500

    Add files via upload

--- Menu.js
+++ Menu.js
@@ -1,9 +1,15 @@
-addEventListener('DOMContentLoaded', () => {
-    const hamburguesa = document.querySelector('.hamburguesa');
-    if(hamburguesa){
-        hamburguesa.addEventListener('click', () => {
-            const menuItems = document.querySelector('.menuItems');
-            menuItems.classList.toggle('show');
-        })
-    }
-})
+const menuItems = document.querySelector(".menuItems");
+const CloseMenu = document.querySelector(".CloseMenu");
+const hamburguesa = document.querySelector(".hamburguesa");
+
+hamburguesa.addEventListener("click", show);
+CloseMenu.addEventListener("click", close);
+
+function show() {
+    menuItems.style.display = "flex";
+    menuItems.style.top = "150px";
+}
+
+function close() {
+    menuItems.style.top = "-100%";
+}
